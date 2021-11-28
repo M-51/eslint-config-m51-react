634694bdd51e951f83f593feacf7b40cdc0a7162
m-51 <m51@51.pl>
2021-11-28 22:32:23 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# eslint-config-m51
+My eslint config
+
+
+## Usage
+
+* create .eslintrc file with: `{ "extends": ["eslint-config-m51"], "root": true }`
---
+++ index.js
@@ -0,0 +1,45 @@
+module.exports = {
+    env: { browser: true, es2021: true, node: true },
+    extends: ["eslint-config-airbnb"].map(require.resolve),
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module"
+    },
+    rules: {
+        indent: ["error", 4],
+        "max-len": ["error", { code: 150, ignoreStrings: true, ignoreTemplateLiterals: true, ignoreComments: true }],
+        "no-restricted-syntax": ["off", "ForOfStatement"],
+        "import/no-extraneous-dependencies": ["error", {devDependencies: true}],
+        "import/extensions": ["error", "ignorePackages"]
+    },
+    ignorePatterns: [
+        "dist/**/*",
+        "build/**/*",
+        "temp/**/*"
+    ],
+    overrides: [
+        {
+            files: ["**/*.{ts,tsx}"],
+            env: { browser: true, es2020: true, node: true },
+            extends: ["eslint-config-airbnb-typescript"].map(require.resolve),
+            parser: "@typescript-eslint/parser",
+            parserOptions: {
+                ecmaVersion: 2021,
+                sourceType: "module",
+                project: "./tsconfig.json"
+            },
+            rules: {
+                "@typescript-eslint/indent": ["error", 4],
+                "@typescript-eslint/type-annotation-spacing": ["error", { overrides: { returnType : { before : true, after : true }}}],
+                "@typescript-eslint/consistent-type-imports": ["error"],
+                "max-len": ["error", { "code": 150, "ignoreStrings": true, "ignoreTemplateLiterals": true, "ignoreComments": true }],
+                "no-restricted-syntax": ["off", "ForOfStatement"],
+                "import/no-extraneous-dependencies": ["error", {"devDependencies": true}],
+                "import/extensions": ["error", "ignorePackages"],
+                "react/require-default-props": ["off"],
+                "react/jsx-indent" : ["error", 4],
+                "react/jsx-indent-props": ["error", 4]
+            }
+        }
+    ]
+}
